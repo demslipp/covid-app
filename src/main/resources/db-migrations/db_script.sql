81c72307fad7e509ca67086822141ca67be654f9
evgeny <krotoveann@gmail.com>
2021-06-01 16:45:56 +0300

    add liquibase

--- src/main/resources/db-migrations/db_script.sql
+++ src/main/resources/db-migrations/db_script.sql
@@ -1,19 +1,17 @@
-CREATE TABLE public.sheet
-(
-    id        BIGSERIAL PRIMARY KEY,
-    user_id   VARCHAR    NOT NULL,
-    s3_bucket VARCHAR   NOT NULL,
-    s3_id     VARCHAR   NOT NULL,
-    updated   timestamp,
-    created   timestamp NOT NULL DEFAULT now()
+CREATE TABLE public.sheet(
+    user_id      VARCHAR   NOT NULL PRIMARY KEY,
+    sheet_id     VARCHAR   NOT NULL UNIQUE,
+    content_type VARCHAR   NOT NULL,
+    s3_bucket    VARCHAR   NOT NULL,
+    updated      timestamp,
+    created      timestamp NOT NULL DEFAULT now()
 );
 
-CREATE TABLE public.qr_sheet_link
-(
-    sheet_id    BIGINT REFERENCES public.sheet (id) ON DELETE CASCADE,
-    user_id     BIGINT    NOT NULL,
-    created     timestamp NOT NULL DEFAULT now(),
-    expiring_at timestamp NOT NULL
+CREATE TABLE public.qr_sheet_link(
+    qr_id       VARCHAR PRIMARY KEY,
+    sheet_id    VARCHAR REFERENCES public.sheet (sheet_id) ON DELETE CASCADE,
+    user_id     VARCHAR REFERENCES public.sheet (user_id) NOT NULL,
+    created     timestamp                                 NOT NULL DEFAULT now(),
+    expiring_at timestamp                                 NOT NULL
 );
-
 
